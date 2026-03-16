3a628d7f113f62e7bb4b699f6e5e3eebce4e3fb6
Yan Lepri <20251CAS10010011@estudantes.ifpr.edu.br>
2026-03-16 13:07:09 -0300

    feat(db): turn user email unique

--- assets/scripts/migrations/001_create_user_table.up.sql
+++ assets/scripts/migrations/001_create_user_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE user (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
-    email TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME,
